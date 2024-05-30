84739fe318f5b7ae288a9775e1fc0a03d940a697
Adam Wheatley <wheatley.adam@me.com>
2024-05-30 08:15:49 -0500

    create home screen

---
+++ lib/start_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+              width: 300, color: const Color.fromARGB(150, 255, 255, 255)),
+          const SizedBox(height: 50),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 18,
+            ),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                side: const BorderSide(color: Colors.white)),
+            onPressed: () {},
+            label: const Text(
+              'Start Quiz',
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+// Container(
+//           decoration: const BoxDecoration(
+//             color: Colors.purple,
+//           ),
+//           child: Center(
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Image.asset('assets/images/quiz-logo.png'),
+//               ],
+//             ),
+//           ),
+//         ),
