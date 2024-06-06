e659c26e126c811803915cc7b7885200c9a25857
Adam Wheatley <wheatley.adam@me.com>
2024-06-06 08:08:40 -0500

    componentiz home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz/start_screen.dart';
+import 'package:flutter_quiz/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [
-                    Color.fromRGBO(136, 2, 194, 1),
-                    Color.fromARGB(255, 96, 2, 136)
-                  ]),
-            ),
-            child: const StartScreen()),
-      ),
-    ),
+    const Quiz(),
   );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(136, 2, 194, 1),
+                    Color.fromARGB(255, 96, 2, 136)
+                  ]),
+            ),
+            child: const StartScreen()),
+      ),
+    );
+  }
+}
